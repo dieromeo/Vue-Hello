570a6c04b01933758d3dc8b745179487ebcacb9c
dieromeo <diegoromeo99@icloud.com>
2023-11-24 15:03:07 +0100

    feat: adding css

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ createApp({
     data() {
         return {
             message: 'Ciao sono vue js',
-            source: 'https://picsum.photos/600/400'
+            source: 'https://picsum.photos/600/400',
+            centrato: 'd-center',
+            testoCentrato: 'text-center'
         }
     }
 }).mount('#app')
